2b549ad5a17576c15b9171383fb48b7b22a97b03
Jeweells <abraham.pacheco6319@gmail.com>
2019-02-23 18:36:59 -0400

    Menu, Warning and Menu functionalities
    Menu, Warning and Menu functionalities (Clicking on Play, Instructions, etc, works)

---
+++ Assets/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuController : MonoBehaviour {
+
+
+    public void Play()
+    {
+        SceneManager.LoadSceneAsync("Play");
+    }
+
+    public void Instructions()
+    {
+        SceneManager.LoadSceneAsync("Instructions");
+    }
+
+    public void Expansions()
+    {
+        SceneManager.LoadSceneAsync("Expansions");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
